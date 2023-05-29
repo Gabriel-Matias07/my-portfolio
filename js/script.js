7a26359dae631e213c6894b6402a8cf7362443af
Gabriel <gabrielpereiramatias07@gmail.com>
2023-05-29 16:00:35 -0300

    "Melhorias em relação ao último repositório"

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i) =>
+        setTimeout(() => (el.innerHTML += letter), 30 * i)
+    );
+}
+
+typeWriter(elementEl);
