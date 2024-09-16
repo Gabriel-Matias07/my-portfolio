033185be58a67ff8c19b3a757c4630974dc42e14
Gabriel <gabrielpereiramatias07@gmail.com>
2024-09-15 21:46:15 -0300

    Atualização do about me text e js animação

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function typeWriter(el) {
     const textArray = el.innerHTML.split('');
     el.innerHTML = '';
     textArray.forEach((letter, i) =>
-        setTimeout(() => (el.innerHTML += letter), 23 * i)
+        setTimeout(() => (el.innerHTML += letter), 17 * i)
     );
 }
 
